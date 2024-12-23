42a2b2d7eed1aa2d591587572fbabf0578f4a3ec
Ismam Hasan Ovi <ismamhasanovi@gmail.com>
2024-12-24 01:03:15 +0600

    Updated

---
+++ Batch_39_Final/2_(d).java
@@ -0,0 +1,30 @@
+package Batch_39_Final;
+
+// public class InvalidVoterException extends Exception {
+//     public InvalidVoterException(String message) {
+//         super(message);
+//     }
+// }
+
+public class TestCustomException {
+    public void validateAge(int age) throws InvalidVoterException {
+        if (age < 18) {
+            throw new InvalidVoterException("InvalidVoterException: age is not enough to vote.");
+        } else {
+            System.out.println("Age is valid for voting.");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestCustomException test = new TestCustomException();
+
+        try {
+            test.validateAge(20);
+            test.validateAge(16);
+        } catch (InvalidVoterException e) {
+            System.out.println("Found the exception");
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+}
+
